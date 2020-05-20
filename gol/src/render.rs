2206b2039f27a37b9fce4302cf38b3c43f2e8eef
Valink16 <valuthyk@gmail.com>
2020-05-21 00:07:59 +0200

    Now draws the grid outline so we can know where the cells are

--- gol/src/render.rs
+++ gol/src/render.rs
@@ -11,6 +11,7 @@ pub struct Renderer {
     pub g: grid::Grid,
     pub window: RenderWindow,
     cells_vbuffer: VertexArray, // Vertex buffer containing all the quads representing the cells
+    grid_vbuffer: VertexArray, // Outline of the grid
     cell_size: Rect<i32>
 }
 
@@ -26,7 +27,8 @@ impl Renderer {
         );
 
         let cells_vbuffer = create_vbuffer(&g.cells, (w, h), (g.w as u32, g.h as u32));
-        
+        let grid_vbuffer = create_grid_vbuffer((w, h), (g.w as u32, g.h as u32));
+
         let cell_size = Rect::<i32> {
             left: 0,
             top: 0,
@@ -38,7 +40,8 @@ impl Renderer {
             g,
             window,
             cells_vbuffer,
-            cell_size
+            cell_size,
+            grid_vbuffer
         }
     }
 
@@ -71,6 +74,7 @@ impl Renderer {
     
             self.window.clear(Color::WHITE);
             self.window.draw(&self.cells_vbuffer);
+            self.window.draw(&self.grid_vbuffer);
             self.window.display();
         }
     }
@@ -137,6 +141,31 @@ fn create_vbuffer(data: &Vec<(bool, bool)>, screen_size: (u32, u32), grid_size: 
         arr[_i + 3].color = c;
 
          // println!("{:?}, {:?}, {:?}, {:?}", arr[_i], arr[_i + 1], arr[_i + 2], arr[_i + 3]);
+    }
+
+    arr
+}
+
+// Creates and returns line vertex buffer representing a grid
+fn create_grid_vbuffer(screen_size: (u32, u32), grid_size: (u32, u32)) -> VertexArray {
+    let mut arr = VertexArray::new(sfml::graphics::PrimitiveType::Lines, ((grid_size.0 + 1) * (grid_size.1 + 1)) as usize);
+    let c_w = (screen_size.0 / grid_size.0) as f32;
+    let c_h = (screen_size.1 / grid_size.1) as f32;
+
+    for y in 0..grid_size.1 + 1 {
+        let _i = (y * 2) as usize;
+        arr[_i].position = Vector2f::new(0.0, y as f32 * c_h);
+        arr[_i + 1].position = Vector2f::new(screen_size.0 as f32, y as f32 * c_h);
+    }
+
+    for x in 0..grid_size.0 + 1 {
+        let _i = ((grid_size.1 + 1) * 2 + x * 2) as usize;
+        arr[_i].position = Vector2f::new(x as f32 * c_w, 0.0);
+        arr[_i + 1].position = Vector2f::new(x as f32 * c_w, screen_size.1 as f32);
+    }
+
+    for i in 0..arr.vertex_count() {
+        arr[i].color = Color::BLACK;
     }
 
     arr
